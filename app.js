67ee0203e2e2848d3f0635b6644091eacbd52272
patelabhay672 <patelabhay0302@gmail.com>
2025-05-11 22:13:03 +0530

    Create new task which is realtime tracker

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+
+const socketio = require("socket.io");
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.send("Heyy");
+});
+
+server.listen(3000);
